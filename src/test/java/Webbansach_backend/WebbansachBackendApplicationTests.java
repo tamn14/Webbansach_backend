2827de76ad80ff5ceeabdf550187a6c7bcbb3dc6
tamn14 <Tamn0443@gmail.com>
2025-03-17 23:29:09 +0700

    Tao du an moi

---
+++ src/test/java/Webbansach_backend/WebbansachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Webbansach_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
